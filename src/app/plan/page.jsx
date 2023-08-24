d3966e18e6fb04b8e2c8487c9a16bd826f23dfd5
longtv <tranvietlong2k@gmail.com>
2023-08-24 12:17:45 +0700

    add login handle destination page

--- src/app/plan/page.jsx
+++ src/app/plan/page.jsx
@@ -1,21 +1,18 @@
 import React from "react";
+import { useRouter } from "next/navigation";
+import TripPlan from "@/components/tripPlan"
 
-function page() {
+const Plan = () => {
+  // const router =. useRouter();
+  // const data = router.query;
+
+  // console.log(data);
+
   return (
-    <section className="flex flex-col justify-center items-center px-[15%] py-[20px] text-center">
-      <div className="w-1/2 border border-transparent backdrop-filter backdrop-blur-md inset-0 -m-30 z-10">
-        <p className="font-bold text-2xl my-5">Trip to Hanoi</p>
-        <div className="text-slate-500">18/8-29/9</div>
-      </div>
-
-      <div className="w-full p-20">
-        <div className="flex justify-between">
-          <p className="font-bold text-xl">Place to visit</p>
-          <div className="text-slate-500">18/8-29/9</div>
-        </div>
-      </div>
+    <section>
+      <TripPlan />
     </section>
   );
-}
+};
 
-export default page;
+export default Plan;
